218c4aaad7276a442e0297a79fa37c4dcf8691fa
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2014-08-26 12:10:36 +0200

    New version

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name(gvterm).
 title('Show Prolog terms using graphviz').
-version('1.0').
+version('1.1').
 author('Jan Wielemaker', 'J.Wielemaker@vu.nl').
 download('http://www.swi-prolog.org/download/pack/gvterm-*.tgz').
